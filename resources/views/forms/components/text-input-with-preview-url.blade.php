c2d77a7057ab4c26aae88b92716c24927af29354
Muhammad Farkhan Syafii <zheninjas75@yahoo.co.id>
2024-10-08 16:01:42 +0700

    update: form input with preview url

--- resources/views/forms/components/text-input-with-preview-url.blade.php
+++ resources/views/forms/components/text-input-with-preview-url.blade.php
@@ -1,21 +1,26 @@
 <x-dynamic-component :component="$getFieldWrapperView()" :field="$field">
-    <div x-data="{
-        prependText: '',
-        required: false,
-        state: $wire.{{ $applyStateBindingModifiers("\$entangle('{$getStatePath()}')") }},
-        init() {
-            this.required = '{{ $field->getRequiredValidationRule() }}' === 'required';
-            this.prependText = '{{ $field->getPrependText() }}' === '' ? '{{ env('APP_URL') }}/' : '{{ $field->getPrependText() }}';
-        },
-        slugify() {
-            return this.prependText + this.state.toLowerCase().trim().replace(/[\s-]+/g, '-').replace(/[^\w-]+/g, '').replace(/--+/g, '-').replace(/^-+|-+$/g, '');
-        }
-    }">
-
+    <div x-data="formInput">
         <x-filament::input.wrapper>
             <x-filament::input type="text" x-model="state" x-bind:required="required" />
         </x-filament::input.wrapper>
 
         <p x-text="slugify()" clas="mt-1"></p>
     </div>
+
+    @script
+    <script>
+        Alpine.data('formInput', () => ({
+            prependText: '',
+            required: false,
+            state: $wire.$entangle('{{ $getStatePath() }}'),
+            init() {
+                this.required = '{{ $field->getRequiredValidationRule() }}' === 'required';
+                this.prependText = '{{ $field->getPrependText() }}' === '' ? '{{ env('APP_URL') }}/' : '{{ $field->getPrependText() }}';
+            },
+            slugify() {
+                return this.prependText + this.state.toLowerCase().trim().replace(/[\s-]+/g, '-').replace(/[^\w-]+/g, '').replace(/--+/g, '-').replace(/^-+|-+$/g, '');
+            }
+        }))
+    </script>
+    @endscript
 </x-dynamic-component>
